0aa0e76e87e28979c79299bc41a087aac309957e
Anoop Narayanan <anarayanan@prosper.com>
2015-11-04 21:43:12 -0800

    Check Prime number initial commit

--- Prime Number/ViewController.swift
+++ Prime Number/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var inputField: UITextField!
+    @IBOutlet var result: UILabel!
+    @IBAction func checkIfPrime(sender: AnyObject) {
+        
+        var isPrime = true
+        let number = Int(inputField.text!)
+        
+        if(number != nil){
+
+            if( number == 1){
+                isPrime = false
+            }
+            
+            if (number != 1 && number != 2) {
+                
+                for (var i = 2 ; i < number; i++) {
+                    if (number! % i == 0) {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if(isPrime){
+                result.text = "Is a Prime Number"
+            } else {
+                result.text = "Not a Prime Number"
+            }
+            
+        } else {
+            result.text = "Not a Valid Number"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
